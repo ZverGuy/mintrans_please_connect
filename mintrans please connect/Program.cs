f37326cd485aaccb68926df866aed788b8dd58c4
ZverGuy <max.bel_2003@mail.ru>
2022-08-25 16:53:29 +0300

    1

---
+++ mintrans please connect/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace mintrans_please_connect
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            var outputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Usage.png");
+            var fileInfo = new FileInfo(outputFile);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+            using var browserFetcher = new BrowserFetcher();
+            await browserFetcher.DownloadAsync();
+            await using var browser = await Puppeteer.LaunchAsync(
+                new LaunchOptions { Headless = true });
+            await using var page = await browser.NewPageAsync();
+            await page.GoToAsync("http://www.google.com");
+            await page.ScreenshotAsync(outputFile);
+        }
+    }
+}
